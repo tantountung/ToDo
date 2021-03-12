4225594ee551f2770d06a4664908531945118036
tanto untung <untung2k3@yahoo.com>
2021-03-12 14:02:43 +0100

    no 4 done

---
+++ ToDo.Tests/Model/TodoTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using ToDo.Model;
+using Xunit;
+
+namespace ToDo.Tests.Model
+{
+    
+    public class TodoTest
+    {
+        [Fact]
+        public void TodoInitTest()
+        {
+            //Arrange
+            int tesTodoId = 54321;
+            string tesDescription = "mopping";
+
+            //Act
+            Todo todo = new Todo(tesTodoId, tesDescription);
+
+            //Assert
+            Assert.Equal(tesTodoId, todo.TodoId);
+            Assert.Equal(tesDescription, todo.Description);
+
+
+
+        }
+        [Fact]
+        public void TodoInitTest1()
+        {
+            //Arrange
+            int tesTodoId = 5678;
+            string tesDescription = null;
+
+            //Act
+            Todo todo = new Todo(tesTodoId, tesDescription);
+
+            //Assert
+            Assert.Equal(tesTodoId, todo.TodoId);
+            Assert.Equal(tesDescription, todo.Description);
+
+
+
+        }
+    }
+}
--- ToDo/Model/Todo.cs
+++ ToDo/Model/Todo.cs
@@ -1,10 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
+using ToDo;
 
 namespace ToDo.Model
 {
-    class Todo
+
+    /*Create Todo class in the model folder
+    a.Required private fields are todoId(int and readonly), description(String), done(bool) 
+    and assignee(Person).
+    b.Make a constructor that take in todoId(int) and a description(String).
+    c.Unit test Todo class.
+    d.Commit changes.*/
+
+    public class Todo
     {
+        private readonly int todoId;
+        private string description;
+        private bool done;
+        private Person assignee;
+
+        public Todo(int todoId, string description)
+        {
+            this.todoId = todoId;
+            this.description = description;
+        }
+
+        public int TodoId
+        {
+            get { return todoId; }
+        }
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+
+
+        }
     }
 }
