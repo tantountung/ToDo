30a7307e7804b44de0b3c26022a3e3fb28d5fdcb
tanto untung <untung2k3@yahoo.com>
2021-03-12 16:11:11 +0100

    no 6

---
+++ ToDo.Tests/Data/PersonSequencerTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using ToDo.Data;
+
+namespace ToDo.Tests.Data
+{
+    public class PersonSequencerTest
+    {
+        [Fact]
+        public void PersonSeqInitTest()
+        {
+            //Arrange
+            int tesId = 123;
+            int tesIdNew = 124;
+
+            //Act
+            int result = PersonSequencer.NextPersonId(tesId);
+
+            //Assert
+            Assert.Equal(tesIdNew, result);
+        }
+        [Fact]
+        public void PersonSeqInitTest1()
+        {
+            //Arrange
+            int tesId = 123;
+            
+
+            //Act
+            int result = PersonSequencer.Reset(tesId);
+
+            //Assert
+            Assert.Equal(0, result);
+        }
+    }
+}
--- ToDo/Data/PersonSequencer.cs
+++ ToDo/Data/PersonSequencer.cs
@@ -4,7 +4,34 @@ using System.Text;
 
 namespace ToDo.Data
 {
-    class PersonSequencer
+    /*
+     Create a new class PersonSequencer in Data folder.
+a. In PersonSequencer creates a private static int variable called personId.
+b. 
+d. Unit test PersonSequencer.
+e. Commit changes.
+    */
+
+    public class PersonSequencer
     {
+   //     Add a static method called nextPersonId that increment and return the next personId value.
+   
+
+        private static int personId;
+        public static int NextPersonId(int personId)
+        {
+            
+            personId = personId + 1;
+            return personId;
+
+        }
+
+        //c.Add a static method called reset() that sets the personId variable to 0.
+        public static int Reset(int personId)
+        {
+            personId = 0;
+            return personId;
+
+        }
     }
 }
